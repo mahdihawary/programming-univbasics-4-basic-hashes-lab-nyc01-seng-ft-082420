4f9e29d3bf1cd07e853411b74114e8731e0f2562
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 03:21:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
- {}
+ hash = {}
 end
 
 def my_hash
- Hash
+ { fav_movie: "District9"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ {:name => "Grace Hopper"}
 end
 
 
